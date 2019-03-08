47d33a83711ca8c041caf203ab52b28d528cd394
Stephan <stephanedmonson@gmail.com>
2019-03-07 23:16:10 -0500

    Added user registartion, middleware for parsing, and gravatar

--- server.js
+++ server.js
@@ -1,32 +1,35 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const express = require("express");
 const mongoose = require("mongoose");
+const bodyParser = require("body-parser");
 
 const users = require("./routes/api/users");
 const profile = require("./routes/api/profile");
 const posts = require("./routes/api/posts");
 
-
 const app = express();
+
+//Body parse middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 //DB config
 const db = process.env.mongoURI;
 
 //connect to MongoDB
 mongoose
-  .connect(db,{ useNewUrlParser: true })
+  .connect(db, { useNewUrlParser: true })
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
 app.get("/", (req, res) => res.send("Hello !"));
 
 // Use routes
-app.use('/api/users', users);
-app.use('/api/profile', profile);
-app.use('/api/posts', posts);
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
-
